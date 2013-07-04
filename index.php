b53efe3322a603ab76bf409c740fe0fb568d0a40
Daktyl198 <rootme198@hotmail.com>
2013-07-04 07:20:48 -0400

    Update index.php
    Fixed typo and added code to check login cookies against a database

--- index.php
+++ index.php
@@ -1,32 +1,34 @@
 <?php
 /*
- * SPOTS - Simple Project Organization Toool for Scanlation
+ * SPOTS - Simple Project Organization Tool for Scanlation
  * 
  * Version: 0.0.0
  */
 
-// Connect to the database
+// Connect to the database or show error upon failure to connect
 $dbconnect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-// Show error upon failure to connect
 if (!$dbconnect) {
     die('Connection Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
 }
 
-// Check if the user is logged in, and if so set an associative array for easy access to their information || TODO Check cookies against the database
+// Check if the user is logged in, and if so set an associative array for easy access to their information
 if (isset($_COOKIE['SPOTS_USER']) && isset($_COOKIE['SPOTS_PASS'])) {
-	$loggedIn = true;
+	$passwordCheck = mysqli_fetch_array(mysqli_query($dbconnect, 'SELECT userPass FROM users WHERE userName ='. $_COOKIE['SPOTS_USER']));
 	
-	$userInfoRaw = mysqli_fetch_array(mysqli_query($dbconnect, 'SELECT * FROM users WHERE userName = '.$_COOKIE['SPOTS_USER']));
-	$userInfo = array(
-		'userID' => $userInfoRaw['0'],
-		'userName' => $userInfoRaw['1'],
-		'userRole' => $userInfoRaw['3'],
-		'userEmail' => $userInfoRaw['4'],
-		'userTitle' => $userInfoRaw['5'],
-	);
-	unset($userInfoRaw); // Destroy the raw array to use as little memory as possible.
+	if ($_COOKIE['SPOTS_PASS'] === $passwordCheck[0]) {
+		$loggedIn = true;
+		
+		$userInfoRaw = mysqli_fetch_array(mysqli_query($dbconnect, 'SELECT * FROM users WHERE userName = '. $_COOKIE['SPOTS_USER']));
+		$userInfo = array(
+			'userID' => $userInfoRaw['0'],
+			'userName' => $userInfoRaw['1'],
+			'userRole' => $userInfoRaw['3'],
+			'userEmail' => $userInfoRaw['4'],
+			'userTitle' => $userInfoRaw['5'],
+		);
+		unset($userInfoRaw); // Destroy the raw array to use as little memory as possible.
+	}
 }
-
 
 if ($loggedIn) {
 	
