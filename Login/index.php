0f74a0d98e34eb726f28d406ade08b80dd049875
koroshiya <korocresent@gmail.com>
2013-07-04 18:54:34 +1000

    Added a few stored procedures, modified index.php to reduce repeat code in regards to "remember me"

--- Login/index.php
+++ Login/index.php
@@ -7,23 +7,18 @@ if (isset($_COOKIE['PK_User']) && isset($_COOKIE['PK_Pass'])) {
 }
 
 if (isset($_POST['PK_User']) && isset($_POST['PK_Pass'])) {
+	//TODO: Add call to Stored Procedure in DB to validate login
+	
+	$time = time() + 60 * 60;
 	if ($_POST['rememberme'] == 'on') {
-		// Create cookies for 1 year here
-		$year = time()+60*60*24*365;
-		setcookie('PK_User', $_POST['PK_User'], $year, '/', '', false, false);
-		setcookie('PK_Pass', $_POST['PK_Pass'], $year, '/', '', false, false);
+		$time = $time*24*365; // Create cookies for 1 year
+	} 
+	setcookie('PK_User', $_POST['PK_User'], $time, '/', '', false, false);
+	setcookie('PK_Pass', $_POST['PK_Pass'], $time, '/', '', false, false);
+	
+	echo '<meta content="0; url = ../" http-equiv="refresh">';
+	exit();
 		
-		echo '<meta content="0; url = ../" http-equiv="refresh">';
-		exit();
-	} else {
-		// Create cookies for 1 hour here
-		$hour = time()+60*60;
-		setcookie('PK_User', $_POST['PK_User'], $hour, '/', '', false, false);
-		setcookie('PK_Pass', $_POST['PK_Pass'], $hour, '/', '', false, false);
-		
-		echo '<meta content="0; url = ../" http-equiv="refresh">';
-		exit();
-	}
 }
 
 ?>
--- sql/SPMS_02_stored_procedures.sql
+++ sql/SPMS_02_stored_procedures.sql
@@ -3,8 +3,14 @@
 *Author: Koro
 *Date created: 04/July/2012
 *Date last modified: 04/July/2012
+*Version: 1.01
 *Changelog: 
+			1.01: Added delete_user, is_founder, is_webmaster, get_user_task_count
 */
+
+--UserIO
+
+--insert_user(userName, userPassword, userRole[, email, title])
 
 DELIMITER // 
 CREATE PROCEDURE IF NOT EXISTS insert_user(IN userName varchar(30), IN userPassword varchar(20), IN userRole character, IN email varchar(50), IN title character) RETURNS boolean
@@ -21,9 +27,57 @@ RETURN true;
 END // 
 DELIMITER ;
 
+--delete_user(userID)
+
+DELIMITER // 
+CREATE PROCEDURE IF NOT EXISTS delete_user(IN userID smallint unsigned) RETURNS boolean
+BEGIN 
+IF is_founder(userID) OR is_webmaster(userID) OR get_user_task_count(userID) > 0
+	RETURN false;
+ELSE 
+	DELETE FROM ScanUser AS su WHERE su.userID = userID;
+	RETURN true;
+END IF;
+END // 
+DELIMITER ;
+
+
+
+--Authentication
+
+DELIMITER // 
+CREATE PROCEDURE IF NOT EXISTS is_founder(IN userID smallint unsigned) RETURNS boolean
+BEGIN 
+DECLARE suUserID smallint unsigned;
+SELECT founderID INTO suUserID FROM Config;
+RETURN SELECT suUserID = userID;
+END // 
+DELIMITER ;
+
+DELIMITER // 
+CREATE PROCEDURE IF NOT EXISTS is_webmaster(IN userID smallint unsigned) RETURNS boolean
+BEGIN 
+DECLARE suUserID smallint unsigned;
+SELECT webmasterID INTO suUserID FROM Config;
+RETURN SELECT suUserID = userID;
+END // 
+DELIMITER ;
+
+
+
+--Getters
+
 DELIMITER // 
 CREATE PROCEDURE IF NOT EXISTS get_project_count() AS smallint unsigned
 BEGIN
 RETURN COUNT(*) FROM Series;
 END // 
 DELIMITER ;
+
+DELIMITER // 
+CREATE PROCEDURE IF NOT EXISTS get_user_task_count(IN userID smallint unsigned) AS smallint unsigned
+BEGIN
+RETURN COUNT(*) FROM Task AS t WHERE t.userID = userID;
+END // 
+DELIMITER ;
+
