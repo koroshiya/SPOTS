1f9da38f48ade6fc933defeb5a0a614189535090
Nathan <rootme198@hotmail.com>
2013-07-04 18:45:18 -0400

    Clean up functions_mysql.php just a bit

--- sql/functions_mysql.php
+++ sql/functions_mysql.php
@@ -2,34 +2,32 @@
 
 function addUser($userName, $userPassword, $userRole, $email=null, $title=null) {
 	mysqli_query($dbconnect, "INSERT INTO users VALUES ($userName, $userPassword, $userRole, $email, $title)");
+	return true;
 }
 
 function removeUser($userName) {
 	mysqli_query($dbconnect, "DELETE * FROM users WHERE userName = $userName");
+	return true;
 }
 
 function is_founder($userName) {
-	array $userID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT userID FROM users WHERE userName = $userName"));
-	array $founderID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT founderID FROM config"));
+	$userID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT userID FROM users WHERE userName = $userName"));
+	$founderID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT founderID FROM config"));
 	if ($userID['0'] === $founderID['0']) {
-		$results = true;
+		return true;
 	} else {
-		$results = false;
+		return false;
 	}
-	
-	return $results;
 }
 
 function is_webmaster($userName) {
-	array $userID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT userID FROM users WHERE userName = $userName"));
-	array $webmasterID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT webmasterID FROM config"));
+	$userID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT userID FROM users WHERE userName = $userName"));
+	$webmasterID = mysqli_fetch_array(mysqli_query($dbconnect, "SELECT webmasterID FROM config"));
 	if ($userID['0'] === $webmasterID['0']) {
-		$results = true;
+		return true;
 	} else {
-		$results = false;
+		return false;
 	}
-	
-	return $results;
 }
 
 ?>
