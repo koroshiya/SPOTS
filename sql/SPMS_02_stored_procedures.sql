21e36d4da86489c2c2901a8275c63b08a66bc46a
koroshiya <korocresent@gmail.com>
2013-07-05 15:33:43 +1000

    All stored procedures now work (as long as the database has been built, populated, etc.) Util.php updated as outlined in changelog. Add and delete user not yet tested.
    Nothing

--- sql/SPMS_02_stored_procedures.sql
+++ sql/SPMS_02_stored_procedures.sql
@@ -3,10 +3,11 @@
 *Author: Koro
 *Date created: 04/July/2012
 *Date last modified: 05/July/2012
-*Version: 1.02
+*Version: 1.03
 *Changelog: 
 			1.01: Added delete_user, is_founder, is_webmaster, get_user_task_count
 			1.02: Fixed and tested get_user_task_count and get_project_count. Others not working or untested.
+			1.03: Altered get_user_task_count and get_project_count. Fixed others.
 */
 
 --UserIO
@@ -14,34 +15,32 @@
 --insert_user(userName, userPassword, userRole[, email, title])
 
 DELIMITER // 
-DROP PROCEDURE IF EXISTS insert_user //
-CREATE PROCEDURE insert_user(IN userName varchar(30), IN userPassword varchar(20), IN userRole character, IN email varchar(50), IN title character, OUT boolResult boolean)
+DROP FUNCTION IF EXISTS insert_user //
+CREATE FUNCTION insert_user(userName varchar(30), userPassword varchar(20), userRole character, email varchar(50), title character) RETURNS boolean
 BEGIN 
 DECLARE totalUsers smallint unsigned;
-SELECT COUNT(*) INTO totalUsers FROM ScanUser;
-IF totalUsers = 65535
-	boolResult = FALSE;
-ELSE
-	boolResult = TRUE;
-END IF;
-
 DECLARE sha1Password binary(20);
-sha1Password = UNHEX( SHA1 (CONCAT (userName, userpassword, 'myEpicSalt', email), 256));
-
-INSERT INTO ScanUser Values(userName, sha1Password, userRole, COALESCE(email, NULL), COALESCE(title, NULL));
+SELECT COUNT(*) INTO totalUsers FROM ScanUser;
+IF totalUsers = 65535 THEN
+	RETURN false;
+ELSE
+	SET sha1Password = UNHEX(SHA1(CONCAT(userName, userPassword, 'myEpicSalt', email)));
+	INSERT INTO ScanUser VALUES(userName, sha1Password, userRole, COALESCE(email, NULL), COALESCE(title, NULL));
+	RETURN true;
+END IF;
 END // 
 DELIMITER ;
 
 --delete_user(userID)
 
 DELIMITER // 
-DROP PROCEDURE IF EXISTS delete_user //
-CREATE PROCEDURE delete_user(IN userID smallint unsigned) RETURNS boolean
+DROP FUNCTION IF EXISTS delete_user //
+CREATE FUNCTION delete_user(userID smallint unsigned) RETURNS boolean
 BEGIN 
-IF is_founder(userID) OR is_webmaster(userID) OR get_user_task_count(userID) > 0
+IF is_founder(userID) OR is_webmaster(userID) OR get_user_task_count(userID) > 0 THEN
 	RETURN false;
 ELSE 
-	DELETE FROM ScanUser AS su WHERE su.userID = userID;
+	DELETE FROM ScanUser WHERE ScanUser.userID = userID;
 	RETURN true;
 END IF;
 END // 
@@ -52,22 +51,26 @@ DELIMITER ;
 --Authentication
 
 DELIMITER // 
-DROP PROCEDURE IF EXISTS is_founder //
-CREATE PROCEDURE is_founder(IN userID smallint unsigned) RETURNS boolean
+DROP FUNCTION IF EXISTS is_founder //
+CREATE FUNCTION is_founder(userID smallint unsigned) RETURNS boolean
 BEGIN 
 DECLARE suUserID smallint unsigned;
+DECLARE boolResult boolean;
 SELECT founderID INTO suUserID FROM Config;
-RETURN SELECT suUserID = userID;
+SELECT suUserID = userID INTO boolResult;
+RETURN boolResult;
 END // 
 DELIMITER ;
 
 DELIMITER // 
-DROP PROCEDURE IF EXISTS is_webmaster //
-CREATE PROCEDURE is_webmaster(IN userID smallint unsigned) RETURNS boolean
+DROP FUNCTION IF EXISTS is_webmaster //
+CREATE FUNCTION is_webmaster(userID smallint unsigned) RETURNS boolean
 BEGIN 
 DECLARE suUserID smallint unsigned;
+DECLARE boolResult boolean;
 SELECT webmasterID INTO suUserID FROM Config;
-RETURN SELECT suUserID = userID;
+SELECT suUserID = userID INTO boolResult;
+RETURN boolResult;
 END // 
 DELIMITER ;
 
@@ -76,18 +79,22 @@ DELIMITER ;
 --Getters
 
 DELIMITER // 
-DROP PROCEDURE IF EXISTS get_project_count //
-CREATE PROCEDURE get_project_count(OUT total smallint)
+DROP FUNCTION IF EXISTS get_project_count //
+CREATE FUNCTION get_project_count() RETURNS smallint unsigned
 BEGIN
+DECLARE total smallint unsigned;
 SELECT COUNT(*) INTO total FROM Series;
+RETURN total;
 END // 
 DELIMITER ;
 
 DELIMITER // 
-DROP PROCEDURE IF EXISTS get_user_task_count //
-CREATE PROCEDURE get_user_task_count(IN userID smallint unsigned, OUT total smallint)
+DROP FUNCTION IF EXISTS get_user_task_count //
+CREATE FUNCTION get_user_task_count(userID smallint unsigned) RETURNS smallint unsigned
 BEGIN
+DECLARE total smallint unsigned;
 SELECT COUNT(*) INTO total FROM Task AS t WHERE t.userID = userID;
+RETURN total;
 END // 
 DELIMITER ;
 
