ea5aab74f45c7151576fa7376a3306ec0ca8042e
koroshiya <korocresent@gmail.com>
2014-03-18 13:28:36 +1100

    Unset POST variables on successful login, added SPOTS_user session variable

--- Login.php
+++ Login.php
@@ -2,29 +2,32 @@
 
 if (!fromIndex){die('You must access this through the root index!');}
 
-    require_once('./Database/Connection.php');
+require_once('./Database/Connection.php');
     
-    if (isset($_POST['loginUser']) && isset($_POST['loginPass'])){
+if (isset($_POST['loginUser']) && isset($_POST['loginPass'])){
     	
-        $username = htmlspecialchars($_POST['loginUser']);
-        $password = htmlspecialchars($_POST['loginPass']);
+    $username = htmlspecialchars($_POST['loginUser']);
+    $password = htmlspecialchars($_POST['loginPass']);
+    unset($_POST['loginUser']);
+    unset($_POST['loginPass']);
         
-        connect();
+    connect();
 
-        if ($connection === null || !mysqli_ping($connection)){
-            die('Database connection failed');
-        }else{
-            require_once('./Database/UserIO.php');
-            $valid = userGetPasswordIsValidByName($username, $password);
-            if ($valid){
-                $_SESSION['SPOTS_authorized'] = $valid;
-            }
-        }
-    }elseif (isset($_SESSION['SPOTS_authorized'])){
-        $_SESSION['SPOTS_authorized'] = null;
+    if ($connection === null || !mysqli_ping($connection)){
+        die('Database connection failed');
     }else{
-    	unset($_POST['loginUser']);
-    	unset($_POST['loginPass']);
+        require_once('./Database/UserIO.php');
+        $valid = userGetPasswordIsValidByName($username, $password);
+        if ($valid){
+            $_SESSION['SPOTS_authorized'] = $valid;
+            $_SESSION['SPOTS_user'] = $username;
+        }
     }
+}elseif (isset($_SESSION['SPOTS_authorized'])){
+    $_SESSION['SPOTS_authorized'] = null;
+}else{
+    unset($_POST['loginUser']);
+	unset($_POST['loginPass']);
+}
 
 ?>
