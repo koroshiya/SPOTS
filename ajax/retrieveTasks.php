edd6ba1daba272869d2bed2ae709f10a8ecd0cf3
koroshiya <korocresent@gmail.com>
2014-03-26 13:01:47 +1100

    Fixed function defining logic

--- ajax/retrieveTasks.php
+++ ajax/retrieveTasks.php
@@ -13,28 +13,42 @@ $statusSearch = $_POST['status'];
 DEFINE('databaseDir', dirname(dirname(__FILE__)).'/Database/');
 require_once(databaseDir.'TaskIO.php');
 
-if ($statusSearch != -1){
-	if ($roleSearch != -1){
-		$args = array($roleSearch, $statusSearch);
-		$data = getDefinedTasksByRoleAndStatus($args);
-	}else if ($seriesSearch != -1){
-		$args = array($seriesSearch, $statusSearch);
-		$data = getDefinedTasksByTitleAndStatus($args);
-	}else{
-		$args = array($statusSearch);
-		$data = getDefinedTasksByStatus($args);
-	}
-}else if ($roleSearch != -1){
-	if ($seriesSearch != -1){
-		$args = array($seriesSearch, $roleSearch);
-		$data = getDefinedTasksByTitleAndRole($args);
-	}else{
+if ($statusSearch == -1 && $roleSearch == -1 && $seriesSearch == -1){
+	//get all //disallow?
+	$data = array("N/A");
+}elseif ($statusSearch == -1){
+	if ($roleSearch == -1){
+		$args = array($seriesSearch);
+		$data = getDefinedTasksByTitle($args);
+	}elseif ($seriesSearch == -1){
 		$args = array($roleSearch);
 		$data = getDefinedTasksByRole($args);
+	}else{
+		$args = array($seriesSearch, $roleSearch);
+		$data = getDefinedTasksByTitleAndRole($args);
 	}
-}else if ($seriesSearch != -1){
-	$args = array($seriesSearch);
-	$data = getDefinedTasksByTitle($args);
+}elseif ($roleSearch == -1){
+	if ($statusSearch == -1){
+		$args = array($seriesSearch);
+		$data = getDefinedTasksByTitle($args);
+	}elseif ($seriesSearch == -1){
+		$args = array($statusSearch);
+		$data = getDefinedTasksByStatus($args);
+	}else{
+		$args = array($seriesSearch, $statusSearch);
+		$data = getDefinedTasksByTitleAndStatus($args);
+	}
+}elseif ($seriesSearch == -1){
+	if ($statusSearch == -1){
+		$args = array($roleSearch);
+		$data = getDefinedTasksByRole($args);
+	}elseif ($roleSearch == -1){
+		$args = array($statusSearch);
+		$data = getDefinedTasksByStatus($args);
+	}else{
+		$args = array($roleSearch, $statusSearch);
+		$data = getDefinedTasksByRoleAndStatus($args);
+	}
 }else{
 	$args = array($seriesSearch, $roleSearch, $statusSearch);
 	$data = getFullyDefinedTasks($args);
