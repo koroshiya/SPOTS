ec89260ac44f16b7c110c821605cfaaeb43f49e2
koroshiya <korocresent@gmail.com>
2014-04-22 12:30:46 +1000

    Replaced Authorization stored procedures with meekro functions

--- Database/Authorization.php
+++ Database/Authorization.php
@@ -1,22 +1,19 @@
 <?php
 
 	require_once('Connection.php');
-
-	//If the user is the webmaster, returns true
+	
 	function isAdmin($userID){
-
-		connectToMeekro();
-		$result = DB::query("SELECT is_admin(%i);", $userID);
-		return $result[0];
-		
+		return getUserRole($userID) === 'A';
 	}
 	
-	//If the user is a mod, returns true
 	function isMod($userID){
-
-		connectToMeekro();
-		$result = DB::query("SELECT is_mod(%i);", $userID);
-		return $result[0];
-		
+		return getUserRole($userID) === 'M';
 	}
+
+	function getUserRole($userID){
+		connectToMeekro();
+		$result = DB::query("SELECT su.userRole INTO suUserRole FROM ScanUser AS su WHERE su.userID = %i;", $userID);
+		return $result;
+	}
+
 ?>
